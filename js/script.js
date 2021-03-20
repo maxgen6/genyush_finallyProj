8d485a5e642dfe87acf98d5f08a7efa490942006
unknown <genjob6@gmail.com>
2021-03-20 17:42:25 +0300

    done gallery part, but have one question

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
 
-const showTab = () => {
-    const tabs = document.querySelectorAll('.tab');
-    const tabsContent = document.querySelectorAll('.tab-item');
+    const showTab = () => {
+        const tabs = document.querySelectorAll('.tab');
+        const tabsContent = document.querySelectorAll('.tab-item');
 
-    tabs.forEach((item, index) => {
-        item.addEventListener('click', e => {
-            tabs.forEach(tab => tab.classList.remove('active'))
-            tabsContent.forEach(content => content.classList.remove('active'));
-            e.target.classList.add('active');
-            tabsContent[index].classList.add('active');
+        tabs.forEach((item, index) => {
+            item.addEventListener('click', e => {
+                tabs.forEach(tab => tab.classList.remove('active'))
+                tabsContent.forEach(content => content.classList.remove('active'));
+                e.target.classList.add('active');
+                tabsContent[index].classList.add('active');
+            })
         })
-    })
-};
+    };
 
-showTab();
+    showTab();
+
+    const showModal = () => {
+        const modal = document.querySelector('.modal'),
+            closeModal = document.querySelector('.modal-close'),
+            img = document.querySelectorAll('.tab-item img');
+        
+
+        img.forEach(item => {
+            item.addEventListener('click', e => {
+                modal.classList.add('modal-active');
+                document.querySelector('.modal-content img').src = e.target.src;
+            })
+        });
+       
+        function close() {
+            modal.classList.remove('modal-active')
+        };
+
+        closeModal.addEventListener('click', close);
+        modal.addEventListener('click', close);
+
+    };
+
+    showModal();
+
+
+});
